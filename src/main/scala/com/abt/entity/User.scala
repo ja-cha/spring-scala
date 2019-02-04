2a13fc0cc9004565a8141e44cc8f6f157ac28978
Jan Abt <jan.abt@smithsonianchannel.com>
2019-02-04 09:21:53 -0500

    commented on usage of @BeanProperty at Scala/JPA intersection

--- src/main/scala/com/abt/entity/User.scala
+++ src/main/scala/com/abt/entity/User.scala
@@ -12,9 +12,15 @@ import scala.beans.BeanProperty
 @Entity
 case class User(name: String, birthDate: Date) {
 
+  /**
+    * The JPA framework is interacting with this class via it's id property.
+    * It expects a bean structured in accordance to the Java Bean convention i.e., with getter and setter methods.
+    * For this reason we annotate the id field with {@link scala.beans.BeanProperty}.
+    *
+    */
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
-  @BeanProperty //generate getter and setter method according to Java standard
+  @BeanProperty
   var id: Long = _
 
 
